4ea64f27ab95702b5f42bea27a46af453093a203
sam1rrrr <54107548+sam1rrrr@users.noreply.github.com>
2020-03-24 20:18:55 +0300

    init commit
    GLOBAL UPDATEEE

---
+++ script.js
@@ -0,0 +1,87 @@
+const BLACKLISTED_KEY_CODES = [ 38 ];
+const COMMANDS = {
+  help:
+    'Команды : <span class="code">about</span>, <span class="code">contact</span>, <span class="code">game</span>, <span class="code">draw</span>, <span class="code">barsik</span>, <span class="code">clear</span>' ,
+  about:
+    "Copyright © Nedosite 2019-2020",
+  
+  
+  contact:   
+  "Соцсети:<br><a href='https://vk.com/sam1rrrr' class='success link'>Vkontakte</a>, <a href='https://www.github.com/sam1rrrr' class='success link'>Github</a>, <a href='https://www.t.me/sam1rrrr' class='success link'>Telegram</a>",
+  login : 'Введите пароль : ',
+  clear : '',
+  icheater12 : 'Вы успешно вошли в систему!',
+  game : 'Запуск игры ...',
+  draw : '',
+  barsik : ''
+};
+
+let userInput, terminalOutput;
+
+const app = () => {
+  userInput = document.getElementById("userInput");
+  terminalOutput = document.getElementById("terminalOutput");
+  document.getElementById("dummyKeyboard").focus();
+  console.log("Application loaded");
+};
+
+const execute = function executeCommand(input) {
+  let output;
+  input = input.toLowerCase();
+  if (input.length === 0) {
+    return;
+  }
+  output = `<div class="terminal-line"><span class="success">➜</span> <span class="directory">~</span> ${input}</div>`;
+  if (!COMMANDS.hasOwnProperty(input)) {
+    output += `<div class="terminal-line">Нет такой команды : ${input}</div>`;
+    console.log("Такой команды нет!");
+  } else {
+    output += COMMANDS[input];
+  }
+  if(input == 'clear'){
+    location.href=location.href;
+  }
+  if(input == 'game'){
+    document.location.href = "./game.html";
+  }
+  if(input == 'barsik'){
+    document.location.href = "./barsik.html";
+  }
+  if(input == 'draw'){
+    document.location.href = "./draw.html";
+  }
+  terminalOutput.innerHTML = `${
+    terminalOutput.innerHTML
+  }<div class="terminal-line">${output}</div>`;
+  terminalOutput.scrollTop = terminalOutput.scrollHeight;
+};
+
+const key = function keyEvent(e) {
+  const input = userInput.innerHTML;
+
+  if (BLACKLISTED_KEY_CODES.includes(e.keyCode)) {
+    return;
+  }
+
+  if (e.key === "Enter") {
+    execute(input);
+    userInput.innerHTML = "";
+    return;
+  }
+
+  userInput.innerHTML = input + e.key;
+};
+
+const backspace = function backSpaceKeyEvent(e) {
+  if (e.keyCode !== 8 && e.keyCode !== 46) {
+    return;
+  }
+  userInput.innerHTML = userInput.innerHTML.slice(
+    0,
+    userInput.innerHTML.length - 1
+  );
+};
+
+document.addEventListener("keydown", backspace);
+document.addEventListener("keypress", key);
+document.addEventListener("DOMContentLoaded", app);
