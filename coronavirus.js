72ea6cb4c57386246dbfdf8669c547562a6739cc
sam1rrrr <54107548+sam1rrrr@users.noreply.github.com>
2020-04-03 22:38:41 +0300

    corona commit

---
+++ coronavirus.js
@@ -0,0 +1,15 @@
+function russia() {
+    document.getElementById("russia").style.display = "block";
+    document.getElementById("world").style.display = "none";
+    document.getElementById("TATARSTAN").style.display = "none";
+}
+function world() {
+    document.getElementById("russia").style.display = "none";
+    document.getElementById("world").style.display = "block";
+    document.getElementById("TATARSTAN").style.display = "none";
+}
+function tatarstan() {
+    document.getElementById("russia").style.display = "none";
+    document.getElementById("world").style.display = "none";
+    document.getElementById("TATARSTAN").style.display = "block";
+}
